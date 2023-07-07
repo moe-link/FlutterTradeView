5574007e0d0b28ca08457ec1a46534be87ad0d6c
moesama_zzt <moesama_zzt@163.com>
2023-07-07 18:37:23 +0800

    初始化代码

---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Entellitrade ${DateTime.now().year}.${DateTime.now().month}.${DateTime.now().day}');
+    setWindowMinSize(const Size(1000, 600));
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  String selectedChoice = 'Categories'; // 初始选择为 element
+  List<String> dataList = []; // 存储接口返回的数据列表
+  List<String> elementList  = []; // element list
+  List<String> productList = [];  // product list
+  List<String> shippingList = []; // shipping list
+  String selectedItem = ''; // 选中的列表项
+  List<int> mList = [1,2 ,3];
+
+  void selectChoice(String choice) {
+    setState(() {
+      selectedChoice = choice;
+      // 在选择改变时调用接口获取数据
+      fetchData();
+    });
+  }
+
+  Future<void> fetchData() async {
+    // TODO: 调用接口获取数据
+    // 模拟异步获取数据，这里使用 Future.delayed 来模拟接口调用
+    await Future.delayed(const Duration(seconds: 1));
+
+    // 假设接口返回的数据是一个字符串列表
+    List<String> newData = ['Item 1', 'Item 2', 'Item 3'];
+
+    setState(() {
+      dataList = newData;
+    });
+  }
+
+  void handleItemClick(String item) {
+    setState(() {
+      selectedItem = item;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Container(
+            width: 300,
+            color: Colors.grey[200],
+            child: Column(
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        selectChoice('Categories');
+                      },
+                      child: Container(
+                        width: 150,
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        decoration: BoxDecoration(
+                          color: selectedChoice == 'Categories' ? Colors.blue : Colors.grey[200],
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Categories',
+                            style: TextStyle(
+                              color: selectedChoice == 'Categories' ? Colors.white : Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        selectChoice('Shippings');
+                      },
+                      child: Container(
+                        width: 150,
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        decoration: BoxDecoration(
+                          color: selectedChoice == 'Shippings' ? Colors.blue : Colors.grey[200],
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Shippings',
+                            style: TextStyle(
+                              color: selectedChoice == 'Shippings' ? Colors.white : Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ExpansionPanelList(
+                        expansionCallback: (panelIndex,isExpanded){
+                          setState(() {
+                            // currentPanelIndex=(currentPanelIndex!=panelIndex?panelIndex:-1);
+                          });
+                        },
+                        children: [
+                          ExpansionPanel(
+                                headerBuilder: (context,isExpanded){
+                                  return const ListTile(
+                                    title: Text('element'),
+                                  );
+                                },
+                                body:const Padding(
+                                  padding: EdgeInsets.all(30.0),
+                                  child:ListBody(
+                                    children: <Widget>[
+                                      Text('這是標題的內容'),
+                                    ],
+                                  ),
+                                ),
+                                isExpanded: false,
+                          ),
+                          ExpansionPanel(
+                            headerBuilder: (context,isExpanded){
+                              return const ListTile(
+                                title: Text('product'),
+                              );
+                            },
+                            body:const Padding(
+                              padding: EdgeInsets.all(30.0),
+                              child: ListBody(
+                                children: <Widget>[
+                                  Text('這是標題的內容'),
+                                ],
+                              ),
+                            ),
+                            isExpanded: false,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.grey[300],
+              child: Center(
+                child: Text(
+                  selectedItem,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
